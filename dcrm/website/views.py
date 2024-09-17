e468ccc8039b96bd2b393f1278ff8669cafa9dd1
collegeGent <rgent@rnngroup.ac.uk>
2024-09-17 07:58:13 +0100

    Added register function

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from .forms import CreateUSerForm, LoginForm
 # Create your views here.
 def home(request):
     return render(request, 'website/index.html')
+
+def register(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #return redirect('')
+    context = {"form": form}
+
+    return render(request,'website/register.html',context=context)
+    
