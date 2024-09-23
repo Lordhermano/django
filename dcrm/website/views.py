a9924799d97cf438507f2b5d535a17bec33a35f0
Lordhermano <160242165@student.rnngroup.ac.uk>
2024-09-23 09:45:41 +0100

    updating work

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from .forms import CreateUSerForm, LoginForm
+from .forms import CreateUserForm,LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+
+
+
 # Create your views here.
 def home(request):
     return render(request, 'website/index.html')
@@ -14,4 +19,23 @@ def register(request):
     context = {"form": form}
 
     return render(request,'website/register.html',context=context)
+
+def my_login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data = request.POST)
+        if form.is_valid():
+        
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request,username = username, password = password)
+
+            if user is not None:
+                auth.login(request,user)          
+                 #return redirect('')
+    
+    
+    context = {"login_form": form}
+
+    return render(request,'website/my-login.html',context=context)    
     
