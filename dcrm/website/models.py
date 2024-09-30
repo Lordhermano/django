9897d812d36bcb536afdaef3beeb0904ebac843b
Lordhermano <160242165@student.rnngroup.ac.uk>
2024-09-30 09:13:35 +0100

    models updated

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+
+    creation_data = models.DateTimeField(auto_now_add=True)
+
+    first_name = models.CharField(max_length=100)
